e00a87dad8bf0363b2d8c9ab8bacbaaeec0bab13
n1xx1n <ndinevski5@gmail.com>
2023-08-23 17:16:49 +0200

    adding Scheduler

--- channel_statistics/apps.py
+++ channel_statistics/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class ChannelStatisticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'channel_statistics'
+
+    # TODO : FIX SCHEDULER
+    # def ready(self):
+    #     import os
+    #     from . import jobs
+
+        # RUN_MAIN check to avoid running the code twice since manage.py runserver runs 'ready' twice on startup
+        # if os.environ.get('RUN_MAIN', None) != 'true':
+        #     jobs.start()
