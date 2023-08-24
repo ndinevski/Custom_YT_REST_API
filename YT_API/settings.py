d3fcf796a614409fc91d030a532cd2634b6d8238
n1xx1n <ndinevski5@gmail.com>
2023-08-25 00:51:59 +0200

    add Postgres

--- YT_API/settings.py
+++ YT_API/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'YT_API.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database', 
+        'USER': 'postgres',
+        'PASSWORD': 'n1xx1n',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
