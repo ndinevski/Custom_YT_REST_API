591ab33ee2991a5ace8cdd5f9950d704b3a24f06
n1xx1n <ndinevski5@gmail.com>
2023-08-26 12:53:49 +0200

    logout and home feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import{
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
 }from 'react-router-dom'
 
 import './App.css';
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,24 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,  useNavigate } from 'react-router-dom'
 
 const NavBar = ({parentToChild}) => {
+    let navigate = useNavigate();
+
+    const logout = () => {
+        navigate("logout");
+    }
+
+
   return (
     <div>
         <div className="stats-header, app-header">
             {parentToChild}
             <div className="navbarlist">
-                <Link to="/">
-                    <h3 className="navbar">Home</h3>
-                </Link>
+                <a href="http://127.0.0.1:8000" className="navbar">Home</a>
                 <Link to="/statistics">
                     <h3 className="navbar">Statistics</h3>
                 </Link>
                 <Link to="/videos">
                     <h3 className="navbar">Videos</h3>
                 </Link>
-                <Link to="/logout">
-                    <h3 className="navbar separatetitle">Logout</h3>
-                </Link>
+                <a href="http://127.0.0.1:8000/logout" className="navbar separatetitle">Logout</a>
             </div>
         </div>    
     </div>
